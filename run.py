8e2409374defd2c42acb079b1fabd4f0d1f33929
WinneonSword <winneonsword@gmail.com>
2013-09-14 13:27:23 -0500

    Gotta love syntax

--- run.py
+++ run.py
@@ -5,19 +5,18 @@ defaultConfig = {
         'username': '',
         'password': '',
         'alert': '',
-		},
+        },
 }
 
 def writeConfig(conf):
-	config = json.dumps(conf, indent = 4, sort_keys = True)
-	with open('config.json', 'w') as f:
-		f.write(config)
-
+    config = json.dumps(conf, indent = 4, sort_keys = True)
+    with open('config.json', 'w') as f:
+        f.write(config)
 
 if __name__ == "__main__":
-	if not os.path.isfile('config.json'):
-		writeConfig(defaultConfig)
-		print("[ wsRTB ] - Created default configuration. Please edit the values before you start this again.")
+    if not os.path.isfile('config.json'):
+        writeConfig(defaultConfig)
+        print("[ wsRTB ] - Created default configuration. Please edit the values before you start this again.")
 	elif 'updateconf' in sys.argv:
 		with open('config.json', 'r') as f:
 			config = json.loads(f.read())
