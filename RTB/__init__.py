c717f96a2d6a2d875f03c991f1184ae1c0959af8
WinneonSword <winneonsword@gmail.com>
2013-09-14 22:34:14 -0500

    Added banned subreddits ( mainly for avodiing NSFW subreddits )

--- RTB/__init__.py
+++ RTB/__init__.py
@@ -19,6 +19,12 @@ def handleRateLimit(func, *args):
 def main():
 	version = "v1.1"
 	
+	bannedSubs = set()
+	bannedSubs.add('gonewild')
+	bannedSubs.add('GoneWildPlus')
+	bannedSubs.add('porn')
+	bannedSubs.add('nsfw')
+	
 	userAgent = (
 		"/u/WinneonSword's beloved RandomTriviaBot, " + version +
 		"For more: http://github.com/WinneonSword/RandomTriviaBot ( Coming soon! )"
@@ -53,19 +59,27 @@ def main():
 			hasWord = any(string in selfPost for string in activationWords)
 			hasTitle = any(string in selfTitle for string in activationWords)
 			if submission.id not in cache and hasWord:
-				temp = [(selfPost.find(i), i) for i in activationWords if i in selfPost]
-				word = min(temp)[1]
-				print("\tFound valid post at submission id '" + submission.id + "'! Adding comment...")
-				handleRateLimit(submission.add_comment, message(word))
-				cache.append(submission.id)
-				print("\tComment posted!")
-				break
+				if submission.subreddit not in bannedSubs:
+					temp = [(selfPost.find(i), i) for i in activationWords if i in selfPost]
+					word = min(temp)[1]
+					print("\tFound valid post at submission id '" + submission.id + "'! Adding comment...")
+					handleRateLimit(submission.add_comment, message(word))
+					cache.append(submission.id)
+					print("\tComment posted!")
+					break
+				else:
+					print("\tThe subreddit '" + submission.subreddit + "' is in the bannedSubs list!")
+					break
 			if submission.id not in cache and hasTitle:
-				print("\tFound valid post at submission id of '" + submission.id + "'! Adding comment...")
-				temp = [(selfTitle.find(i), i) for i in activationWords if i in selfTitle]
-				word = min(temp)[1]
-				handleRateLimit(submission.add_comment, message(word))
-				cache.append(submission.id)
-				print("\tComment posted!")
-				break
+				if submission.subreddit not in bannedSubs:
+					print("\tFound valid post at submission id of '" + submission.id + "'! Adding comment...")
+					temp = [(selfTitle.find(i), i) for i in activationWords if i in selfTitle]
+					word = min(temp)[1]
+					handleRateLimit(submission.add_comment, message(word))
+					cache.append(submission.id)
+					print("\tComment posted!")
+					break
+				else:
+					print("\tThe subreddit '" + submission.subreddit + "' is in the bannedSubs list!")
+					break
 		time.sleep(30)
