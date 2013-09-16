ae63e500aebbcbf9d8957a3d1eea3ccdfbe8dda3
WinneonSword <winneonsword@gmail.com>
2013-09-15 21:38:59 -0500

    Added a keyboardinterrupt catch, and a few other things

--- RTB/__init__.py
+++ RTB/__init__.py
@@ -13,8 +13,8 @@ def handleRateLimit(func, *args):
 			func(*args)
 			break
 		except praw.errors.RateLimitExceeded as error:
-			print("\tRate limit exceeded! Sleeping for %d seconds" % error.sleep_time)
-			time.sleep(error.sleep_time)
+			print("\tRate limit exceeded! Ignoring! THIS IS NOT A GOOD IDEA.")
+			break
 
 def main():
 	version = "v1.1"
@@ -37,7 +37,7 @@ def main():
 	
 	userAgent = (
 		"/u/WinneonSword's beloved RandomTriviaBot, " + version +
-		"For more: http://github.com/WinneonSword/RandomTriviaBot ( Coming soon! )"
+		"For more: http://github.com/WinneonSword/RandomTriviaBot"
 	)
 	
 	r = praw.Reddit(user_agent = userAgent)
@@ -62,36 +62,39 @@ def main():
 	cache = []
 	
 	activationWords = ['cactus', 'rofl', 'banana', 'wine', 'tomato', 'pineapple', 'dolphin']
-	while True:
-		print("[ wsRTB ] - Fetching new posts...")
-		subreddit = r.get_subreddit("all")
-		for submission in subreddit.get_new(limit = 30):
-			selfPost = submission.selftext.lower()
-			selfTitle = submission.title.lower()
-			hasWord = any(string in selfPost for string in activationWords)
-			hasTitle = any(string in selfTitle for string in activationWords)
-			if submission.id not in cache and hasWord:
-				if submission.subreddit.display_name not in bannedSubs:
-					temp = [(selfPost.find(i), i) for i in activationWords if i in selfPost]
-					word = min(temp)[1]
-					print("\tFound valid post at submission id '" + submission.id + "'! Adding comment...")
-					handleRateLimit(submission.add_comment, message(word))
-					cache.append(submission.id)
-					print("\tComment posted!")
-					break
-				else:
-					print("\tThe subreddit '" + submission.subreddit.display_name + "' is in the bannedSubs list!")
-					break
-			if submission.id not in cache and hasTitle:
-				if submission.subreddit.display_name not in bannedSubs:
-					print("\tFound valid post at submission id '" + submission.id + "'! Adding comment...")
-					temp = [(selfTitle.find(i), i) for i in activationWords if i in selfTitle]
-					word = min(temp)[1]
-					handleRateLimit(submission.add_comment, message(word))
-					cache.append(submission.id)
-					print("\tComment posted!")
-					break
-				else:
-					print("\tThe subreddit '" + submission.subreddit.display_name + "' is in the bannedSubs list!")
-					break
-		time.sleep(30)
+	try:
+		while True:
+			print("[ wsRTB ] - Fetching new posts...")
+			subreddit = r.get_subreddit("all")
+			for submission in subreddit.get_new(limit = 30):
+				selfPost = submission.selftext.lower()
+				selfTitle = submission.title.lower()
+				hasWord = any(string in selfPost for string in activationWords)
+				hasTitle = any(string in selfTitle for string in activationWords)
+				if submission.id not in cache and hasWord:
+					if submission.subreddit.display_name not in bannedSubs:
+						temp = [(selfPost.find(i), i) for i in activationWords if i in selfPost]
+						word = min(temp)[1]
+						print("\tFound valid post at submission id '" + submission.id + "'! Adding comment...")
+						handleRateLimit(submission.add_comment, message(word))
+						cache.append(submission.id)
+						print("\tComment posted!")
+						break
+					else:
+						print("\tThe subreddit '" + submission.subreddit.display_name + "' is in the bannedSubs list!")
+						break
+				if submission.id not in cache and hasTitle:
+					if submission.subreddit.display_name not in bannedSubs:
+						print("\tFound valid post at submission id '" + submission.id + "'! Adding comment...")
+						temp = [(selfTitle.find(i), i) for i in activationWords if i in selfTitle]
+						word = min(temp)[1]
+						handleRateLimit(submission.add_comment, message(word))
+						cache.append(submission.id)
+						print("\tComment posted!")
+						break
+					else:
+						print("\tThe subreddit '" + submission.subreddit.display_name + "' is in the bannedSubs list!")
+						break
+			time.sleep(30)
+	except KeyboardInterrupt as error:
+		print("[ wsRTB ] - Stopped RandomTriviaBot!")		
